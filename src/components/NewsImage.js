fb6c8bd9af60bc0c2d2ccd94f67ea672c97b75dc
Zyzz294 <hamzaozgezici294@gmail.com>
2024-06-01 18:39:57 +0600

    Revert "init"
    This reverts commit 897a16016d25f6238ef36455aa29a7119b71f20c.
    
    'undo mistakes'

--- src/components/NewsImage.js
+++ src/components/NewsImage.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
+import img from '../assets/img.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
 import '../App.css'; 
 
 const NewsImage = () => {
@@ -8,35 +11,35 @@ const NewsImage = () => {
     <Carousel.Item>
       <img style={{height:'80vh'}}
         className="d-block w-100"
-        src="https://images.unsplash.com/photo-1617802690658-1173a812650d?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={img}
         alt="First slide"
       />
       <Carousel.Caption>
-        <h3>Innovatech</h3>
+        <h3>TechTrek</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img style={{height:'80vh'}}
         className="d-block w-100"
-        src="https://images.unsplash.com/photo-1605647540924-852290f6b0d5?q=80&w=2037&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={img2}
         alt="Second slide"
       />
 
       <Carousel.Caption>
-        <h3>Innovatech</h3>
+        <h3>TechTrek</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img style={{height:'80vh'}}
         className="d-block w-100"
-        src="https://plus.unsplash.com/premium_photo-1663134285459-0022b0e78970?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={img3}
         alt="Third slide"
       />
 
       <Carousel.Caption>
-        <h3>Innovatech</h3>
+        <h3>TechTrek</h3>
         <p>
           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
         </p>
